b86fa0d98a13b4959a003bf42c52595440543a37
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-01-20 11:45:07 +0530

    Update 47_foreach.cpp

--- 47_foreach.cpp
+++ 47_foreach.cpp
@@ -12,7 +12,7 @@ int main(){
 
 int A[]={3,2,1,6,5,4,9,8,7};
 
-for (int x:A){     
+for (int x:A){     // we can also type "auto" instead of "int" in for loop's paranthesis.
     cout<<++x<<endl; // will print array with 1 added in each element.
 }
 
