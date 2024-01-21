3c39aeb86f91d191f0342c38610bfd07fd9080e7
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-01-21 11:34:11 +0530

    Update 64_adding2mattrix.cpp

--- 64_adding2mattrix.cpp
+++ 64_adding2mattrix.cpp
@@ -32,33 +32,66 @@
 //  }
 
 
-// using 3 variables.
+// using functions for accepting, displaying and adding matrices.
+// adding 2 matrices
 
- #include<iostream>
- using namespace std;
- int main()
- {
-   
-   int a[2][3]={2,5,9,7,3,6};
-   int b[2][3]={6,3,4,9,5,2};
-   int c[2][3];
+#include<iostream>
+using namespace std;
 
- for(int i=0; i<2; i++){
-for(int j=0; j<3; j++){
-
-c[i][j]=a[i][j]+b[i][j];
+// accepting matrix function
+void accept(int arr[3][3]){
+      cout<<"enter elements for matrix: "<<endl;
+      for (int i = 0; i < 3; i++)
+      {
+            for (int j = 0; j < 3; j++)
+            {
+                  cin>>arr[i][j];
+            }
+            
+      }
+      
 }
- }
- 
- for(int i=0; i<2; i++){
-for(int j=0; j<3; j++){
 
-cout<<"\t"<<c[i][j];
-
+// matrix display function
+void display(int arr[3][3]){
+      cout<<"matrix :"<<endl;
+      for (int i = 0; i < 3; i++)
+      {
+            for (int j = 0; j < 3; j++)
+            {
+                  cout<<"\t"<<arr[i][j];
+            }
+            cout<<endl;
+      }
 }
-cout<<endl;
- }
-   
-     return 0;
- }
- // the same goes for remaining operation.
+
+
+// addition function
+
+void add(int a[3][3], int b[3][3]){
+      cout<<"addition : "<<endl;
+      for (int i = 0; i < 3; i++)
+      {
+            for (int j = 0; j < 3; j++)
+            {
+                  cout<<"\t"<<a[i][j]+b[i][j];
+            }
+            cout<<endl;
+      }
+}
+
+
+int main(){
+
+int a[3][3];
+accept(a);
+display(a);
+
+int b[3][3];
+accept(b);
+display(b);
+
+add(a,b);
+           
+return 0;
+}
