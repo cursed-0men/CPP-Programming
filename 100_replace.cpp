353e87508dcb3b025a26ad62b2a2235d682811f2
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-02-01 11:21:33 +0530

    Create 100_replace.cpp

---
+++ 100_replace.cpp
@@ -0,0 +1,15 @@
+// str.replace(index, number of alphabets,"text")
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    string s = "prograkking";
+    cout<<s<<endl;
+
+    s.replace(6,2,"mm");
+    cout<<s<<endl;
+         
+return 0;
+}
