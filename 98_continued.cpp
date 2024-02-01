eb653c89a6527f3b9e0761f3598defeae51d3003
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-02-01 11:19:28 +0530

    Update 98_continued.cpp

--- 98_continued.cpp
+++ 98_continued.cpp
@@ -1,2 +1,18 @@
 // string class functions continued...
-// 122
+// str.append("some text") -> adding string at end
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+string s1 = "hello";
+cout<<s1.capacity()<<endl;
+s1.append(", how are you");
+
+
+cout<<s1<<endl;
+cout<<s1.capacity()<<endl; // capacity after appending will be changed.
+cout<<"length = "<<s1.length()<<endl;
+return 0;
+}
