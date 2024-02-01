e6a1e191339cb8d584dc5320c2509c5d39b1f3ae
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-02-01 11:23:02 +0530

    Update 101_push_pop.cpp

--- 101_push_pop.cpp
+++ 101_push_pop.cpp
@@ -1,5 +1,5 @@
 // str.push_back("single alphabet") -> adds single alphabet at end of string
-// str.pop_back() -> removes single alphabet from end of string/
+// str.pop_back() -> removes single alphabet from end of string
 
 #include<iostream>
 #include<string>
@@ -10,10 +10,10 @@ string s = "helloo";
 cout<<s<<endl;
 
 s.push_back('z');
-cout<<s<<endl;
+cout<<s<<endl; // hellooz
 
 s.pop_back();
-cout<<s<<endl;
+cout<<s<<endl; // helloo
 
 return 0;
 }
